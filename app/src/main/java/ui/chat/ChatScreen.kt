673b82e4096f8e0c0c98bea8f001ecdc0d1cb648
Tạ Viết Phương <83411225+tvphuong2@users.noreply.github.com>
2025-11-11 23:23:40 +0700

    Import systemBars insets companion

--- app/src/main/java/ui/chat/ChatScreen.kt
+++ app/src/main/java/ui/chat/ChatScreen.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.WindowInsets.Companion.systemBars
 import androidx.compose.foundation.layout.WindowInsetsSides
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
